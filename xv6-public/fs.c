f6210881f916137c65cbd3a4dc9014c79b4e7bea
성창호 <oknkc8@gmail.com>
2019-06-08 18:26:44 +0900

    done project double indirect

--- xv6-public/fs.c
+++ xv6-public/fs.c
@@ -396,6 +396,31 @@ bmap(struct inode *ip, uint bn)
     brelse(bp);
     return addr;
   }
+  bn -= NINDIRECT;
+
+  if(bn < NINDIRECT * NINDIRECT){
+	  if((addr = ip->addrs[NDIRECT+1]) == 0)
+		  ip->addrs[NDIRECT+1] = addr = balloc(ip->dev);
+
+	  bp = bread(ip->dev, addr);
+	  a = (uint*)bp->data;
+
+	  if((addr = a[bn / NINDIRECT]) == 0){
+		  a[bn / NINDIRECT] = addr = balloc(ip->dev);
+		  log_write(bp);
+	  }
+	  brelse(bp);
+
+	  bp = bread(ip->dev, addr);
+	  a = (uint*)bp->data;
+
+	  if((addr = a[bn % NINDIRECT]) == 0){
+		  a[bn % NINDIRECT] = addr = balloc(ip->dev);
+		  log_write(bp);
+	  }
+	  brelse(bp);
+	  return addr;
+  }
 
   panic("bmap: out of range");
 }
@@ -409,8 +434,8 @@ static void
 itrunc(struct inode *ip)
 {
   int i, j;
-  struct buf *bp;
-  uint *a;
+  struct buf *bp, *bp2;
+  uint *a, *b;
 
   for(i = 0; i < NDIRECT; i++){
     if(ip->addrs[i]){
@@ -429,6 +454,27 @@ itrunc(struct inode *ip)
     brelse(bp);
     bfree(ip->dev, ip->addrs[NDIRECT]);
     ip->addrs[NDIRECT] = 0;
+  }
+
+  if(ip->addrs[NDIRECT+1]){
+    bp = bread(ip->dev, ip->addrs[NDIRECT+1]);
+    a = (uint*)bp->data;
+    for(i = 0; i < NINDIRECT; i++){
+      if(a[i]){
+        bp2 = bread(ip->dev, a[i]);
+        b = (uint*)bp2->data;
+        for(j = 0; j < NINDIRECT; j++){
+          if(b[j])
+            bfree(ip->dev, b[j]);
+        }
+        brelse(bp2);
+        bfree(ip->dev, a[i]);
+        a[i] = 0;
+      }
+    }
+    brelse(bp);
+    bfree(ip->dev, ip->addrs[NDIRECT+1]);
+    ip->addrs[NDIRECT+1] = 0;
   }
 
   ip->size = 0;
